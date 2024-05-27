f53fc6a3b496b40364cf8f8276def06cf27c6622
Hilary Ehlers <hilarydixson@gmail.com>
2024-05-27 11:48:25 -0500

    functions lab continued

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+}
+console.log(saturdayFun());
+
+const mondayWork = function (activity = "go to the office") {
+    return `This Monday, I will ${activity}.`;
+}
+mondayWork();
+
+function wrapAdjective(flair = "*") {
+    return function(adjective = "special"){
+        return `You are ${flair}${adjective}${flair}!`;
+    }
+}
+wrapAdjective()()
